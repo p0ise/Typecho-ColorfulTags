aacaafec57cf7960dc7bf685680cafbeea1ace46
invelop <webfenglin@163.com>
2020-01-16 22:03:23 +0800

    CSS selector syntax selection, update project homepage, optimize logic, do not turn on 3D effects on article pages

--- Plugin.php
+++ Plugin.php
@@ -1,100 +1,166 @@
 <?php
-
 /**
- * <a href='https://blog.irow.top/archives/396.html' title='mainpage' target='_blank'>彩色3D标签云插件</a>
+ * <a href='https://www.irow.top/archives/396.html' title='项目主页' target='_blank'>彩色3D标签云插件</a>
  *
  * @package ColorfulTags
  * @author 锋临
- * @version 1.1
- * @link https://blog.irow.top/
+ * @version 1.5
+ * @link https://www.irow.top/
  */
-
-class ColorfulTags_Plugin implements Typecho_Plugin_Interface
-{
-    /* 激活插件方法 */
-    public static function activate()
-    {
-      Typecho_Plugin::factory('Widget_Archive')->footer = array(__CLASS__, 'render');
-    }
-
-    /* 禁用插件方法 */
-    public static function deactivate(){}
-
-    /* 插件配置方法 */
-    public static function config(Typecho_Widget_Helper_Form $form)
-    {
-      $tagDiv = new Typecho_Widget_Helper_Form_Element_Text(
-        'tagDiv', NULL, 'colorfultags',
-        _t('标签云容器定位：'),
-        _t('这里写上标签云容器的ID，如果没有ID请自行添加')
-      );
-      $form->addInput($tagDiv);
-      $radius = new Typecho_Widget_Helper_Form_Element_Text(
-        'radius', NULL, '80',
-        _t('标签云半径：'),
-        _t('默认为80，如果不是很清楚请勿修改')
-      );
-      $form->addInput($radius);
-      $speed = new Typecho_Widget_Helper_Form_Element_Text(
-        'speed', NULL, '11',
-        _t('旋转速度：'),
-        _t('默认为11，如果不是很清楚请勿修改')
-      );
-      $form->addInput($speed);
-
-      //$jquery = new Typecho_Widget_Helper_Form_Element_Radio('jquery',
-      //    ['0' => _t('不加载'), '1' => _t('加载')],
-      //    '1', _t('是否加载外部jQuery库'), _t('插件需要jQuery库文件的支持，如果已加载就不需要加载了 jquery源是新浪Public Resources on SAE：https://lib.sinaapp.com/js/jquery/1.9.1/jquery-1.9.1.min.js'));
-      $pjax = new Typecho_Widget_Helper_Form_Element_Radio('pjax', ['0' => _t('否'), '1' => _t('是')], '0', _t('是否启用了PJAX'), _t('如果你启用了pjax,当切换页面时候，js不会重写绑定事件到新生成的节点上。
-你可以在该项设置中重新加载js函数，以便将事件正确绑定ajax生成的DOM节点上'));
-      $form->addInput($pjax);
-    }
-
-    /* 个人用户的配置方法 */
-    public static function personalConfig(Typecho_Widget_Helper_Form $form){}
-
-    /* 插件实现方法 */
-    public static function render(){
-      /*获取参数*/
-      $options = Helper::options();
-      $tagDiv = $options->plugin('ColorfulTags')->tagDiv;
-      $radius = $options->plugin('ColorfulTags')->radius;
-      $speed = $options->plugin('ColorfulTags')->speed;
-      $pjax = $options->plugin('ColorfulTags')->pjax;
-      $src = $options->pluginUrl.'/ColorfulTags/js/colorfultags.min.js';
-
-      /*输出彩色标签云*/
-      echo "<script src='$src'></script>";
-
-      $script = '<script>';
-      if($pjax){
-        $script .= 'window.onload = function(){console.info("%c彩色标签云-承影|BLOG.IROW.TOP","line-height:28px;padding:4px;background:#3f51b5;color:#fff;font-size:14px;font-family:Microsoft YaHei;");};$(document).on("ready pjax:end", '."function() {colorfultags('$tagDiv',$radius,200,Math.PI / 180,1,1,true,$speed,200,0,10,1);});";
-      }else{
-        $script .= "window.onload = function(){
-          colorfultags('$tagDiv',$radius,200,Math.PI / 180,1,1,true,$speed,200,0,10,1);
-          console.info(\"%c彩色标签云-承影|BLOG.IROW.TOP\",\"line-height:28px;padding:4px;background:#3f51b5;color:#fff;font-size:14px;font-family:Microsoft YaHei;\");
-        }";
-      }
-      $script .= '</script>';
-      $tagDiv = '#'.$tagDiv;
-      $css = <<<css
-      <style>
-      {$tagDiv}{position:relative;width:240px;height:240px;border: 2px black;}
-      {$tagDiv} a{position:absolute;color:#fff;text-align:center;text-overflow:ellipsis;white-space:nowrap;top:0;left:0;padding:3px 5px;border:0;border-radius:3px;display:inline-block;line-height:18px;margin:0 10px 15px 0}
-      {$tagDiv} a:hover{background:#d02f53;display:block}
-      {$tagDiv} a:nth-child(8n){background: #f60;}
-      {$tagDiv} a:nth-child(8n+1){background: #45B6F7;}
-      {$tagDiv} a:nth-child(8n+2){background: #15a287;}
-      {$tagDiv} a:nth-child(8n+3){background: #5cb85c;}
-      {$tagDiv} a:nth-child(8n+4){background: #d9534f;}
-      {$tagDiv} a:nth-child(8n+5){background: #567e95;}
-      {$tagDiv} a:nth-child(8n+6){background: #00a67c;}
-      {$tagDiv} a:nth-child(8n+7){background: #b37333;}
-      </style>
+class ColorfulTags_Plugin implements Typecho_Plugin_Interface {
+	/* 激活插件方法 */
+	public static function activate() {
+		Typecho_Plugin::factory('Widget_Archive')->footer = array(__CLASS__, 'render');
+	}
+	/* 禁用插件方法 */
+	public static function deactivate() {
+	}
+	/* 插件配置方法 */
+	public static function config(Typecho_Widget_Helper_Form $form) {
+		$css_selector = new Typecho_Widget_Helper_Form_Element_Text('css_selector', NULL, 'tag_cloud', _t('CSS选择器'), _t('填写标签云父容器的CSS选择器路径'));
+		$form->addInput($css_selector);
+		$is_3d = new Typecho_Widget_Helper_Form_Element_Radio('is_3d', ['0' => _t('否'), '1' => _t('是')], '0', _t('是否启用3D效果'), _t('开启后标签云会围绕3D球体滚动'));
+		$form->addInput($is_3d);
+		$radius = new Typecho_Widget_Helper_Form_Element_Text(
+							'radius', NULL, '80',
+							_t('3D标签云半径：'),
+							_t('默认为80，如果不是很清楚请勿修改')
+						);
+		$form->addInput($radius);
+		$speed = new Typecho_Widget_Helper_Form_Element_Text(
+							'speed', NULL, '11',
+							_t('3D旋转速度：'),
+							_t('默认为11，如果不是很清楚请勿修改')
+						);
+		$form->addInput($speed);
+		$is_pjax = new Typecho_Widget_Helper_Form_Element_Radio('is_pjax', ['0' => _t('否'), '1' => _t('是')], '0', _t('是否启用了PJAX'), _t('如果你启用了pjax,当切换页面时候，js不会重写绑定事件到新生成的节点上。
+			你可以在该项设置中重新加载js函数，以便将事件正确绑定ajax生成的DOM节点上'));
+		$form->addInput($is_pjax);
+	}
+	/* 个人用户的配置方法 */
+	public static function personalConfig(Typecho_Widget_Helper_Form $form) {
+	}
+	/* 插件实现方法 */
+	public static function render() {
+		/*获取参数*/
+		$options = Helper::options();
+		$css_selector = $options->plugin('ColorfulTags')->css_selector;
+		$is_3d = $options->plugin('ColorfulTags')->is_3d;
+		$radius = $options->plugin('ColorfulTags')->radius;
+		$speed = $options->plugin('ColorfulTags')->speed;
+		$is_pjax = $options->plugin('ColorfulTags')->is_pjax;
+		$is_post = $archive->parameter->type=='post';
+		$static_src = $options->pluginUrl.'/ColorfulTags';
+		$basic_style = <<<css
+						<style>
+						{$css_selector}>a {
+							color: #fff;
+							text-align: center;
+							text-overflow: ellipsis;
+							white-space: nowrap;
+							padding: 3px 5px;
+							border: 0;
+							border-radius: 3px;
+							display: inline-block;
+							line-height: 18px;
+						}
+						
+						{$css_selector}>a:hover {
+							background: #d02f53 !important;
+						}
+						</style>
+css;
+		$around_style = <<<css
+				<style>
+				{$css_selector} {
+					position: relative;
+					width: 100%;
+					padding-top: 100%;
+					border: 2px black;
+					margin: 0 10px 15px 0
+				}
+				{$css_selector}>a {
+					position: absolute;
+					top: 0;
+					left: 0;
+					color: #fff;
+					text-align: center;
+					text-overflow: ellipsis;
+					white-space: nowrap;
+					padding: 3px 5px;
+					border: 0;
+					border-radius: 3px;
+					display: inline-block;
+					line-height: 18px
+				}
+				
+				{$css_selector}>a:hover {
+					background: #d02f53 !important;
+					display: block
+				}
+				</style>
 css;
 
-      echo $css;
-      echo $script;
-    }
-
+		if($is_pjax) {
+			if($is_post||!$is_3d) {
+ 				$html = <<<html
+ 							<!-- Start ColorfulTags -->
+ 							{$basic_style}
+ 							<script src="{$static_src}/js/colorfultags.min.js"></script>
+ 							<script id="colorfultags">
+ 							console.info("%c彩色标签云-锋临|WWW.IROW.TOP","line-height:28px;padding:4px;background:#3f51b5;color:#fff;font-size:14px;font-family:Microsoft YaHei;");
+ 							colorfultags("{$css_selector}");
+ 							$($(document).one("pjax:clicked", function() {
+ 								$.pjax.reload('#colorfultags')
+ 							}));
+ 							</script>
+ 							<!-- End ColorfulTags -->
+html;
+			} else {
+ 				$html = <<<html
+ 							<!-- Start ColorfulTags -->
+ 							{$around_style}
+ 							<script src="{$static_src}/js/colorfultags.min.js"></script>
+ 							<script src="{$static_src}/js/around3d.min.js"></script>
+ 							<script id="#colorfultags">
+ 							console.info("%c彩色标签云-锋临|WWW.IROW.TOP","line-height:28px;padding:4px;background:#3f51b5;color:#fff;font-size:14px;font-family:Microsoft YaHei;");
+ 							colorfultags("{$css_selector}");
+ 							around3D("{$css_selector}",{$radius}, 200, Math.PI / 180, 1, 1, true, {$speed}, 200, 0, 10, 1);
+ 							$($(document).one("pjax:clicked", function() {
+ 								$.pjax.reload('#colorfultags')
+ 							}));
+ 							</script>
+ 							<!-- End ColorfulTags -->
+html;
+			}
+		} else {
+			if($is_post||!$is_3d) {
+ 				$html = <<<html
+ 							<!-- Start ColorfulTags -->
+ 							{$basic_style}
+ 							<script src="{$static_src}/js/colorfultags.min.js"></script>
+ 							<script>
+ 							console.info("%c彩色标签云-锋临|WWW.IROW.TOP","line-height:28px;padding:4px;background:#3f51b5;color:#fff;font-size:14px;font-family:Microsoft YaHei;");
+ 							colorfultags("{$css_selector}");
+ 							</script>
+ 							<!-- End ColorfulTags -->
+html;
+			} else {
+ 				$html = <<<html
+ 							<!-- Start ColorfulTags -->
+ 							{$around_style}
+ 							<link rel="stylesheet" type="text/css" href="{$static_src}/css/around3d.min.css">
+ 							<script src="{$static_src}/js/colorfultags.min.js"></script>
+ 							<script src="{$static_src}/js/around3d.min.js"></script>
+ 							<script>
+ 							console.info("%c彩色标签云-锋临|WWW.IROW.TOP","line-height:28px;padding:4px;background:#3f51b5;color:#fff;font-size:14px;font-family:Microsoft YaHei;");
+ 							colorfultags("{$css_selector}");
+ 							around3D("{$css_selector}",{$radius}, 200, Math.PI / 180, 1, 1, true, {$speed}, 200, 0, 10, 1);
+ 							</script>
+ 							<!-- End ColorfulTags -->
+html;
+			}
+		}
+		echo $html;
+	}
 }
--- README.md
+++ README.md
@@ -1,15 +1,14 @@
 # Typecho-ColorfulTags
-彩色3D标签云插件，原生JS实现。
 
-![效果图](https://blog.irow.top/usr/uploads/2019/08/154750410.png)
+彩色3D标签云插件，一键生成好看的彩色3D标签云
+
+![效果图][1]![效果图][2]![效果图][3]
 
 ## 食用方法
 
-解压后重命名文件夹为ColorfulTags，激活插件，在插件设置中设置标签云容器ID，如果没有就手动添加一个再到后台完成设置。
-
-具体可参考[handsome主题安装方式](blog.irow.top/archives/396.html)
-
-
+ 1. 解压后重命名为ColorfulTags文件
+ 2. 在插件设置中设置标签云容器CSS选择器
+![通用版版后台设置][4]
 
 ## 插件地址
 
@@ -17,4 +16,14 @@ Github地址：<https://github.com/invelop/Typecho-ColorfulTags>
 
 码云地址：<https://gitee.com/ETAS/Typecho-ColorfulTags>
 
-博客地址：<https://blog.irow.top/archives/396.html>
+项目主页：<https://www.irow.top/archives/396.html>
+
+## 实现参考
+彩色3D标签云插件，原生JS实现。
+
+原生JS参考：<https://www.irow.top/archives/191.html>
+
+[1]: https://cdn.irow.top/blog/2020/01/1814463442.png
+[2]: https://cdn.irow.top/blog/2020/01/1934395081.png
+[3]: https://cdn.irow.top/blog/2020/01/503948933.png
+[4]: https://cdn.irow.top/blog/2020/01/3560795202.png
